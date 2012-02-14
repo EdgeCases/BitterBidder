b995a8535dfaf50b4dbdbe262045211d5e213680
Michael Gressman <michael_gressman@yahoo.com>
2012-02-13 22:40:03 -0600

    Added comment indicating purpose of custom validator in Bid

--- grails-app/domain/bitterbidder/Bid.groovy
+++ grails-app/domain/bitterbidder/Bid.groovy
@@ -7,6 +7,7 @@ class Bid {
     static  belongsTo = [listing:Listing, bidder: Customer] // B-3, B-6
 
     static constraints = {
+        // B-5: The Bid amount must be at least .50 higher than the previous Bid for the same listing
         amount(validator: { amount, bid ->
             if (bid.listing.bids) {
                 def maxCurrentBid = bid.listing.bids.max( { a, b -> a.amount <=> b.amount } as Comparator ).amount
