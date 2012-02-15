dbe28fa45a0a6668993a21236186a77c58247d26
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-02-14 23:24:48 -0600

    Added validation test for seller and winner.

--- grails-app/domain/bitterbidder/Listing.groovy
+++ grails-app/domain/bitterbidder/Listing.groovy
@@ -1,19 +1,33 @@
 package bitterbidder
 
 class Listing {
-    String description
-    Date endDateTime
     String name
-    Customer seller
     Float startingPrice
+    Date endDateTime
+    String description
     Customer winner
+    //Customer seller
+    static hasMany = [bids:Bid] // B-4
+    static belongsTo = [seller: Customer]
 
-    static hasMany = [bids:Bid]
 
     static constraints = {
-        description (nullable: true, blank: true, empty:true, size: 0..255)
+        description (nullable: true, blank: false, empty:false, size: 0..255)
         name (size: 1..63, empty:false, blank: false)
-        winner (nullable: true)
         endDateTime(min: new Date());
+        bids(minSize: 2)
+        seller(validator: {val, obj ->
+                            println("validating seller")
+                            def isValid = obj.seller.validate()
+                            println("Seller is"+isValid.toString())
+                            return isValid
+                          })
+        winner(nullable: true, validator: {val, obj ->
+            def isValid =true
+            if (obj.winner!=null){
+                isValid = obj.winner.validate()
+            }
+            return isValid
+        })
     }
 }
---
+++ test/integration/bitterbidder/ListingIntegrationTests.groovy
@@ -0,0 +1,17 @@
+package bitterbidder
+
+import static org.junit.Assert.*
+import org.junit.*
+
+class ListingIntegrationTests {
+
+    @Before
+    void setUp() {
+        // Setup logic here
+    }
+
+    @After
+    void tearDown() {
+        // Tear down logic here
+    }
+}
