0c6a1ae0c37410feaa0830b31a19f51afc9942e1
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-02-15 01:08:46 -0600

    Removed bids validation since it doesn't make any sense to have a list of bids when the Listing is first saved.  Attempted to write integration test for listing but it doesn't seem to cascade the bid save.

--- test/integration/bitterbidder/ListingIntegrationTests.groovy
+++ test/integration/bitterbidder/ListingIntegrationTests.groovy
@@ -1,17 +1,47 @@
 package bitterbidder
 
-import static org.junit.Assert.*
 import org.junit.*
+import grails.test.mixin.TestFor
 
+@TestFor(Listing)
 class ListingIntegrationTests {
 
+    Customer validCustomer
+    Listing listingUnderTest
+    Bid aTestBid;
     @Before
     void setUp() {
-        // Setup logic here
+
+        //Setup logic here
+        validCustomer = new Customer(emailAddress: "validguy@valid.com", password: "secret");
+
+        aTestBid = new Bid(amount: 10.50, bidder: validCustomer, dateCreated: new Date())
+        listingUnderTest = new Listing(                   
+                description: "A test listing",
+                seller: validCustomer,                
+                endDateTime: new Date()+1,
+                name: "MyListing",
+                startingPrice: 10)
     }
 
     @After
     void tearDown() {
         // Tear down logic here
     }
+
+    @Test
+    void test_Save_WhenANewBidIsAdded_BidIsSavedWithListing() {
+        //arrange
+        listingUnderTest.bids = new HashSet<Bid>()
+        listingUnderTest.addToBids(aTestBid)
+        //act
+        listingUnderTest.save(flush: true)
+
+        def bid = Bid.get(1)
+        
+        //assert
+        assert bid !=null
+    }
+
+
 }
