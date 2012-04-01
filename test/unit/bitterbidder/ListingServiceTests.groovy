4a7852160f0e75f11cbca14665dc162acb8712f1
unknown <eric@eric-asusPC.(none)>
2012-04-01 13:59:08 -0500

    ecs - added listing and bid services test files

---
+++ test/unit/bitterbidder/ListingServiceTests.groovy
@@ -0,0 +1,17 @@
+package bitterbidder
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(ListingService)
+class ListingServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
