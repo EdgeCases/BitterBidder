512159e78666205df995b4ee55c2172c1bfda647
Danny Alcantara <Danny.Alcantara@gmail.com>
2012-04-08 23:38:30 -0500

    uncommented test

--- test/integration/bitterbidder/EmailServiceIntegrationTests.groovy
+++ test/integration/bitterbidder/EmailServiceIntegrationTests.groovy
@@ -15,32 +15,32 @@ class EmailServiceIntegrationTests {
 
     @After
     void tearDown() {
-        //greenMail.deleteAllMessages()
+        greenMail.deleteAllMessages()
     }
 
-//    def mailService
-//    def emailService
-//    def greenMail
+    def mailService
+    def emailService
+    def greenMail
 
     //Extra credit!
     @Test
     void test_OnMessage_WhenInvoked_EmailIsSent() {
 
-//        //emailService = new EmailService();
-//        //emailService.mailService = mailService
-//        def listing = TestUtility.getValidListingWithBids();
-//
-//        def msg = (listing as XML).toString()
-//
-//        //TODO: fix this after we figure out the serialization problem
-//        emailService.onMessage("A Test Message");
-//        assertEquals(1, greenMail.getReceivedMessages().length)
-//        def message = greenMail.getReceivedMessages()[0]
-//        def expected =EmailMessageHelper.MakeListingWinnerMessage("A Test Message")
-//
-////        def expected =EmailMessageHelper.MakeListingWinnerMessage(listing)
-//        assertEquals(expected, GreenMailUtil.getBody(message))
-//        assertEquals('bitterbidderdev@gmail.com', GreenMailUtil.getAddressList(message.from))
-//        assertEquals('You are a winner!', message.subject)
+        //emailService = new EmailService();
+        //emailService.mailService = mailService
+        def listing = TestUtility.getValidListingWithBids();
+
+        def msg = (listing as XML).toString()
+
+        //TODO: fix this after we figure out the serialization problem
+        emailService.onMessage("A Test Message");
+        assertEquals(1, greenMail.getReceivedMessages().length)
+        def message = greenMail.getReceivedMessages()[0]
+        def expected =EmailMessageHelper.MakeListingWinnerMessage("A Test Message")
+
+//        def expected =EmailMessageHelper.MakeListingWinnerMessage(listing)
+        assertEquals(expected, GreenMailUtil.getBody(message))
+        assertEquals('bitterbidderdev@gmail.com', GreenMailUtil.getAddressList(message.from))
+        assertEquals('You are a winner!', message.subject)
     }
 }
